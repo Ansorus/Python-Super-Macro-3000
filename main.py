97767b57a659c04511d3c08f49f640f503989caf
Ansorus <ansor.games@gmail.com>
2025-11-30 22:37:19 -0800

    Looping and Stopping
    You can now Loop the macro, and press F1 to stop the macro

--- main.py
+++ main.py
@@ -5,29 +5,63 @@ from OptionBox import option_box
 from macro import follow_command, everything
 from tkinter import simpledialog
 from tkinter import messagebox
+from pynput import keyboard
 
 # -- WINDOW SETUP -- #
 window = Tk()
 window.title("Super Macro 3000")
-window.geometry('500x500-25+25')
+#window.geometry('500x500-25+25')
 window.resizable(True,True)
 
 left = ttk.Frame(window,width=25,height=500)
-left.grid(column=0,row=0, rowspan=4) # Change this if grid Y size changes
+left.grid(column=0,row=0, rowspan=5) # Change this if grid Y size changes
+
+right = ttk.Frame(window,width=25,height=500)
+right.grid(column=3,row=0, rowspan=5)
 
 options = [(everything[command]['OptionText'] if 'OptionText' in everything[command] else command) for command in everything.keys()]
 values = [(everything[command]['DefaultValue'] if 'DefaultValue' in everything[command] else command) for command in everything.keys()]
 
 # -- INPUT FUNCTIONS -- #
+loop = 'OFF'
+def toggle_loop():
+    global loop, loop_button
+    loop = 'ON' if loop == 'OFF' else 'OFF'
+    loop_button.config(text='Loop: ' + loop)
+
 def add_command():
     answer = option_box("What command are you adding?", options, values)
     scrolling_frame.new_element(answer)
 
+playing = False
 def play_macro():
     global scrolling_frame
+    global play, playing
+
+    playing = True
+    play.config(text='F1: Stop')
+    play.update()
+
     items = scrolling_frame.get_items()
-    for item in items:
-        follow_command(item)
+    while True:
+        for item in items:
+            follow_command(item)
+            if not playing:
+                break
+        if loop == 'OFF' or not playing:
+            break
+    playing = False
+    play.config(text='Play')
+    play.update()
+
+def stop_macro(key, injected):
+    if not injected:
+        if key == keyboard.Key.f1:
+            global playing
+            playing = False
+
+listener = keyboard.Listener(on_release=stop_macro)
+listener.start()
 
 def edit_element(index, element: str):
     global scrolling_frame
@@ -67,11 +101,15 @@ scrolling_frame = ScrollingFrame(list_box, window, command=edit_element)
 # Buttons
 add = ttk.Button(window, text="Add Command", command=add_command)
 add.grid(column=1,row=1)
-record = ttk.Button(window, text="Cool Button")
-record.grid(column=2,row=1)
+loop_button = ttk.Button(window, text="Loop: OFF", command=toggle_loop)
+loop_button.grid(column=2,row=1)
+
+# F1 to Stop instruction
+label = ttk.Label(window, text='F1 to stop the macro', font=('Arial', 10))
+label.grid(column=1, row=3, columnspan=2)
 
 # Play Button
 play = Button(window, text="Play", command=play_macro)
-play.grid(column=1,row=3,columnspan=2)
+play.grid(column=1,row=4,columnspan=2)
 
 window.mainloop()
